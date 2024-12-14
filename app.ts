f620610897ac26ce4f0520984a5e0017181c483b
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-12-15 01:09:41 +0200

    add vite-based front

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', router);
 app.use(express.static('./front/dist'));
+app.get('*', (_req, res) => { res.sendFile('front/dist/index.html', { root: process.cwd() }) });
 
 app.listen(PORT, () => {
     console.log(`App is running on Port ${PORT}`);
